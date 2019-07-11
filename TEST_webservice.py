3d2a8d3234dc1c83f7e2f71aa5704f8abe0d42de
Johannes Lange <johannes.lange@rwth-aachen.de>
2019-07-11 15:37:56 +0200

    http://httpbin.org -> https://httpbin.org

--- TEST_webservice.py
+++ TEST_webservice.py
@@ -54,42 +54,42 @@ class Test_RestClient:
 
 class Test_JSONRestClient:
 	"""
-	>>> jrc0 = JSONRestClient(url = 'http://httpbin.org/get')
+	>>> jrc0 = JSONRestClient(url = 'https://httpbin.org/get')
 	>>> r0 = response_filter(jrc0.get())
 	>>> cmp_obj(r0, {'headers': {'Accept': 'application/json',
 	...    'Host': 'httpbin.org', 'Content-Type': 'application/json'},
-	...    'args': {}, 'url': 'http://httpbin.org/get'})
+	...    'args': {}, 'url': 'https://httpbin.org/get'})
 	>>> jrc = JSONRestClient()
-	>>> r1 = response_filter(jrc.get('http://httpbin.org/get'))
-	>>> r1 = response_filter(jrc.get('http://httpbin.org/get'))
+	>>> r1 = response_filter(jrc.get('https://httpbin.org/get'))
+	>>> r1 = response_filter(jrc.get('https://httpbin.org/get'))
 	>>> cmp_obj(r1, {'headers': {'Accept': 'application/json',
 	...    'Host': 'httpbin.org', 'Content-Type': 'application/json'},
-	...    'args': {}, 'url': 'http://httpbin.org/get'})
-	>>> r2 = response_filter(jrc.get('http://httpbin.org', 'get', {'header': 'test'}, {'key': 'value'}))
+	...    'args': {}, 'url': 'https://httpbin.org/get'})
+	>>> r2 = response_filter(jrc.get('https://httpbin.org', 'get', {'header': 'test'}, {'key': 'value'}))
 	>>> cmp_obj(r2, {'headers': {'Accept': 'application/json',
 	...    'Host': 'httpbin.org', 'Content-Type': 'application/json', 'Header': 'test'},
-	...    'args': {'key': 'value'}, 'url': 'http://httpbin.org/get?key=value'})
-	>>> r3 = response_filter(jrc.post('http://httpbin.org', 'post', data = {'test': 'value'}))
+	...    'args': {'key': 'value'}, 'url': 'https://httpbin.org/get?key=value'})
+	>>> r3 = response_filter(jrc.post('https://httpbin.org', 'post', data = {'test': 'value'}))
 	>>> cmp_obj(r3, {'headers': {'Accept': 'application/json',
 	...    'Host': 'httpbin.org', 'Content-Type': 'application/json', 'Content-Length': '17'},
-	...    'files': {}, 'form': {}, 'url': 'http://httpbin.org/post', 'args': {},
+	...    'files': {}, 'form': {}, 'url': 'https://httpbin.org/post', 'args': {},
 	...    'json': {'test': 'value'}, 'data': '{"test": "value"}'})
-	>>> r4 = response_filter(jrc.put('http://httpbin.org', 'put', data = {'test': 'value'}))
+	>>> r4 = response_filter(jrc.put('https://httpbin.org', 'put', data = {'test': 'value'}))
 	>>> cmp_obj(r4, {'headers': {'Accept': 'application/json',
 	...    'Host': 'httpbin.org', 'Content-Type': 'application/json', 'Content-Length': '17'},
-	...    'files': {}, 'form': {}, 'url': 'http://httpbin.org/put', 'args': {},
+	...    'files': {}, 'form': {}, 'url': 'https://httpbin.org/put', 'args': {},
 	...    'json': {'test': 'value'}, 'data': '{"test": "value"}'})
-	>>> r5 = response_filter(jrc.delete('http://httpbin.org', 'delete', params = {'key': 'value'}))
+	>>> r5 = response_filter(jrc.delete('https://httpbin.org', 'delete', params = {'key': 'value'}))
 	>>> dummy = r5['headers'].pop('Content-Length', None)
 	>>> cmp_obj(r5, {'headers': {'Accept': 'application/json',
 	...    'Host': 'httpbin.org', 'Content-Type': 'application/json'},
-	...    'files': {}, 'form': {}, 'url': 'http://httpbin.org/delete?key=value', 'args': {'key': 'value'},
+	...    'files': {}, 'form': {}, 'url': 'https://httpbin.org/delete?key=value', 'args': {'key': 'value'},
 	...    'json': None, 'data': ''})
-	>>> try_catch(lambda: jrc.get('http://httpbin.org/getx'), 'RestError', 'Unable to query')
+	>>> try_catch(lambda: jrc.get('https://httpbin.org/getx'), 'RestError', 'Unable to query')
 	caught
-	>>> try_catch(lambda: jrc.get('http://httpbin.org/html'), 'RestError', 'Received invalid JSON reply')
+	>>> try_catch(lambda: jrc.get('https://httpbin.org/html'), 'RestError', 'Received invalid JSON reply')
 	caught
-	>>> try_catch(lambda: jrc.get('http://httpbin.org/bytes/0'), 'RestError', 'Received empty reply')
+	>>> try_catch(lambda: jrc.get('https://httpbin.org/bytes/0'), 'RestError', 'Received empty reply')
 	caught
 	"""
 
